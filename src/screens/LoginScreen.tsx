0e5a624f43a89abfbe2408c346986b9e0fee46e1
sashavershkova <a.m.vershkova@gmail.com>
2025-07-26 09:17:50 -0700

    add remember me option on lockscreen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, Image } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../App';
 import styles from '../styles/loginStyles';
 import api from '../api/axiosInstance';
 import { MaterialIcons } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
 
@@ -12,6 +14,21 @@ const LoginScreen = ({ navigation }: Props) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
+
+  useEffect(() => {
+  const loadSavedUsername = async () => {
+    try {
+      const savedUsername = await AsyncStorage.getItem('savedUsername');
+      if (savedUsername) {
+        setUsername(savedUsername);
+        setRememberMe(true);
+      }
+    } catch (err) {
+      console.log('Error loading saved username:', err);
+    }
+  };
+  loadSavedUsername();
+  }, []);
 
   const handleLogin = async () => {
     try {
@@ -27,6 +44,13 @@ const LoginScreen = ({ navigation }: Props) => {
 
       console.log('✅ Logged in as:', username, '| ID:', id, '| Role:', role);
       navigation.navigate('Home', { userId: id });
+
+      if (rememberMe) {
+        await AsyncStorage.setItem('savedUsername', username);
+      } else {
+        await AsyncStorage.removeItem('savedUsername');
+      }
+      
     } catch (error) {
       console.error('❌ Login failed:', error);
       Alert.alert('Login failed', 'Invalid username. Please try again.');
