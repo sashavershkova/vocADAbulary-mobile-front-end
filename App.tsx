496c148553c1bd6e2afdafbe4aa1bc82b505105f
KateM <ekaterinamarantidi@gmail.com>
2025-07-27 17:01:13 -0700

    Fix double screen

--- App.tsx
+++ App.tsx
@@ -11,7 +11,6 @@ import SplashScreen from './src/screens/SplashScreen';
 import FallbackScreen from './src/screens/FallbackScreen';
 import WalletScreen from './src/screens/WalletScreen';
 import { useFonts } from 'expo-font';
-import AppNavigator from './src/navigation/AppNavigator';
 
 const customFonts = {
   'ArchitectsDaughter': require('./src/assets/fonts/ArchitectsDaughter-Regular.ttf'),
@@ -60,7 +59,6 @@ export default function App() {
           <Stack.Screen name="Fallback" component={FallbackScreen} />
         </Stack.Navigator>
       </NavigationContainer>
-      <AppNavigator />
     </UserProvider>
   );
 }
